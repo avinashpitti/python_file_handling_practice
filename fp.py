9777a5482f5d7f4f2376a0d38e66aa0d8f5aa997
avinashpitti <avinash87129@gmail.com>
2026-01-30 22:48:37 +0530

    add append+ mode

--- fp.py
+++ fp.py
@@ -4,3 +4,21 @@ with open('data.txt','w') as f :
 with open('data.txt','r') as f :
     data = f.read()
     print(data)
+
+with open('info.txt','w+') as f :
+    f.write('I learnt javascript basics today')
+    f.seek(0)
+    data=f.read()
+    print(data)
+
+with open('info.txt','a+') as f :
+    f.write('52 weeks in a year')
+    f.seek(0)
+    data=f.read()
+    print(data)
+
+with open('info.txt','a+') as f :
+    f.write('\nreact is the best javascript framework as of now')
+    f.seek(0)
+    print(f.read())
+
