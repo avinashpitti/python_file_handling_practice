bcd8fe5e9f15f28e6e719b69f63335406132cc6b
avinashpitti <avinash87129@gmail.com>
2026-01-31 18:28:22 +0530

    practice problems using seek() and tell()

--- fp.py
+++ fp.py
@@ -1,15 +1,12 @@
-with open('data.txt','r') as f :
-    for line in f :
-        print(line.strip())
+#1
+with open('info.txt','w+') as f :
+    f.write('Hello Python') 
+    print(f.tell())
+    print(f.seek(0))
+    print(f.read())
 
-with open('data.txt','a') as f :
-    f.write('This is line 4')
-    f.write('This is line 5')
-
-with open('data.txt','a') as f :
-    f.write('\nfunctions')
-    f.write('\nvariables')
-    f.write('\n objects\n')
-
-with open('data.txt','a') as f :
-    f.write('python\nmysql\nfastapi\n')
+#2
+with open('info.txt','r') as f :
+    print(f.readline(4))
+    print(f.seek(0))
+    print(f.read())
