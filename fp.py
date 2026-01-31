0f0167b89d08e5f87c77dd307833fe5d62b163c1
avinashpitti <avinash87129@gmail.com>
2026-01-31 17:51:45 +0530

    line break with append

--- fp.py
+++ fp.py
@@ -5,3 +5,11 @@ with open('data.txt','r') as f :
 with open('data.txt','a') as f :
     f.write('This is line 4')
     f.write('This is line 5')
+
+with open('data.txt','a') as f :
+    f.write('\nfunctions')
+    f.write('\nvariables')
+    f.write('\n objects\n')
+
+with open('data.txt','a') as f :
+    f.write('python\nmysql\nfastapi\n')
