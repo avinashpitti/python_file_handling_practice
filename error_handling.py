3ddd832a39d10215e3cedac5c5a6cca9b10533ab
avinashpitti <avinash87129@gmail.com>
2026-02-01 22:10:43 +0530

    valueerror

---
+++ error_handling.py
@@ -0,0 +1,5 @@
+try :
+    marks=int(input('Enter a number : '))
+    print("your marks : ",marks)
+except ValueError :
+    print("Marks must be a number")
--- users.py
+++ users.py
@@ -9,3 +9,27 @@ print(type(data))
 print(data["name"])
 print(data['age'])
 print(data['skills'][2])
+
+#student.json
+
+data={
+    'name':'Rahul',
+    'age':29,
+    'city':'wayanad'
+}
+
+with open('student.json','w')as f :
+    json.dump(data,f)
+
+
+# students.json
+
+info=[
+    {'name':'Avinash','age':21,'loc':'hyderabad'},
+    {'name':'Srinivas','age':29,'loc':'nalgonda'},
+    {'name':'Amar','age':27,'loc':'Guntur'}
+]
+
+with open('students.json','w') as f :
+    json.dump(data,f)
+
